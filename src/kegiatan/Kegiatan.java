3f7d9ed30166b2242a973c8569bf8d1782d30ca0
titacahyaa <pratistha2602@gmail.com>
2022-01-12 15:53:12 +0700

    memasukan src ke repo git

---
+++ src/kegiatan/Kegiatan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kegiatan;
+
+/**
+ *
+ * @author titacahyaa
+ */
+public class Kegiatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("halo nama ku tita");
+        System.out.println("aku suka olahraga");
+    }
+    
+}
